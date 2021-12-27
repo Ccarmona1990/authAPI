ad1598fb004f6cc90f2d2f0b102da5e676731656
Christopher Carmona <c.carmona0990@gmail.com>
2021-12-26 23:44:49 -0400

    cors update

--- server.mjs
+++ server.mjs
@@ -32,7 +32,7 @@ app.use(session({
 
 app.use(morgan('tiny'));
 app.use(cors({
-    origin: '*',
+    origin: ['http//http://localhost:3000/', 'http://localhost:8080/', 'https://tm-auth-api.herokuapp.com/', 'https://tm-auth-api.herokuapp.com/api/v1/login/', 'https://tm-auth-api.herokuapp.com/api/v1/register/', 'https://tm-auth-api.herokuapp.com/api/v1/tasks/','https://taskm-api.herokuapp.com/', 'https://taskm-api.herokuapp.com/api/v1/tasks' ],
     methods: ["GET", "POST"],
     credentials: true
     })
